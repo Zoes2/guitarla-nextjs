9a6bb4e250e14f57c61875d7f2590965a6eefab7
Zoes2 <v.meneses20.20@gmail.com>
2023-11-11 23:07:39 -0300

    Inicio de proyecto con NextJS, config de rutas

---
+++ pages/nosotros.js
@@ -0,0 +1,14 @@
+import Link from "next/link"
+
+export default function Nosotros() {
+    return (
+        <div>
+
+            <h1>Nosotros</h1>
+            <Link href="/">Inicio</Link>
+
+        </div>
+
+        
+    )
+}
