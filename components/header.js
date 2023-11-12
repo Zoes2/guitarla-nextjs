ac1f27efcf7d3a6030ff1a384005dce33cd7ba04
Zoes2 <v.meneses20.20@gmail.com>
2023-11-12 00:09:06 -0300

    creación vista de tienda y blog, estilos a header

--- components/header.js
+++ components/header.js
@@ -1,20 +1,29 @@
 import Image from "next/future/image"
 import Link from "next/link"
+import styles from '../styles/header.module.css'
 
 export default function Header() {
     return (
 
-        <header>
-            <div className="contenedor">
+        <header className={styles.header}>
+            <div className={`contenedor ${styles.barra}`}>
                 <Image src="/img/logo.svg" alt="logo" width={300} height={40} />
 
-                <nav>
+                <nav className={styles.navegacion}>
                     <Link href="/">
                         Inicio
                     </Link>
 
                     <Link href="/nosotros">
                         Nosotros
+                    </Link>
+
+                    <Link href="/blog">
+                        Blog
+                    </Link>
+
+                    <Link href="/tienda">
+                        Tienda
                     </Link>
                 </nav>
             </div>
---
+++ pages/blog.js
@@ -0,0 +1,13 @@
+import Layout from "../components/layout"
+
+export default function Blog() {
+    return (
+            <Layout
+                tittle={'GuitarLA - Blog'}
+                description={'Blog de música, venta de guitarras, consejos, GuitarLA'}
+            >
+                <h1>Blog</h1>
+            </Layout>
+
+    )
+}
