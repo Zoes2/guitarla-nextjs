4eabe3ed528c50c58158a39227e49bf4e933c7d3
Zoes2 <v.meneses20.20@gmail.com>
2023-11-11 23:31:14 -0300

    config layout, header y footer para las vistas, config etiquetas head

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,16 @@
 import Link from "next/link"
+import Layout from "../components/layout"
 
 export default function Home() {
   return (
     <>
-      <h1>Hola</h1>
-
-      <Link href="/nosotros">Nosotros</Link>
+      <Layout
+        tittle={'Inicio'}
+        description={'Blog de música, venta de guitarras y más'}
+      >
+        <h1>Hola</h1>
+        <Link href="/nosotros">Nosotros</Link>
+      </Layout>
     </>
   )
 }
--- pages/nosotros.js
+++ pages/nosotros.js
@@ -1,14 +1,15 @@
 import Link from "next/link"
+import Layout from "../components/layout"
 
 export default function Nosotros() {
     return (
-        <div>
+            <Layout
+                tittle={'Nosotros'}
+                description={'Sobre nosotros, guitarLA, tienda de música'}
+            >
+                <h1>Nosotros</h1>
+                <Link href="/">Inicio</Link>
+            </Layout>
 
-            <h1>Nosotros</h1>
-            <Link href="/">Inicio</Link>
-
-        </div>
-
-        
     )
 }
