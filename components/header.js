9addda5d50e795dc469e0b924738a1a98e4d9c90
Zoes2 <v.meneses20.20@gmail.com>
2023-11-13 22:41:42 -0300

    config de header y footer en la nav principal,  estilos css con módulos

--- components/header.js
+++ components/header.js
@@ -1,29 +1,45 @@
 import Image from "next/future/image"
 import Link from "next/link"
+import { useRouter } from "next/router"
 import styles from '../styles/header.module.css'
 
 export default function Header() {
+
+    const router = useRouter()
+
     return (
 
         <header className={styles.header}>
             <div className={`contenedor ${styles.barra}`}>
-                <Image src="/img/logo.svg" alt="logo" width={300} height={40} />
+                <Link href={'/'}>
+                    <a>
+                        <Image src="/img/logo.svg" alt="logo" width={300} height={40} />
+                    </a>
+                </Link>
 
                 <nav className={styles.navegacion}>
                     <Link href="/">
-                        Inicio
+                        <a className={router.pathname === '/' ? styles.active : ''}>
+                            Inicio
+                        </a>
                     </Link>
 
                     <Link href="/nosotros">
-                        Nosotros
+                        <a className={router.pathname === '/nosotros' ? styles.active : ''}>
+                            Nosotros
+                        </a>
                     </Link>
 
                     <Link href="/blog">
-                        Blog
+                        <a className={router.pathname === '/blog' ? styles.active : ''}>
+                            Blog
+                        </a>
                     </Link>
 
                     <Link href="/tienda">
-                        Tienda
+                        <a className={router.pathname === '/tienda' ? styles.active : ''}>
+                            Tienda
+                        </a>
                     </Link>
                 </nav>
             </div>
