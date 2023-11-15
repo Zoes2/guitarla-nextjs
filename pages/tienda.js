cc26f23f30005754ea286f794745304e840050b4
Zoes2 <v.meneses20.20@gmail.com>
2023-11-14 23:29:43 -0300

    inyección de estilos mediante módulos para vista tienda y reutilizamos código de proyecto anterior

--- pages/tienda.js
+++ pages/tienda.js
@@ -1,38 +1,35 @@
-import Layout from "../components/layout"
-import Guitarra from "../components/guitarra"
+import Layout from "../components/layout";
+import Guitarra from "../components/guitarra";
+import styles from '../styles/grid.module.css'
 
-export default function Tienda({guitarras}) {
+export default function Tienda({ guitarras }) {
+  return (
+    <Layout
+      tittle={"GuitarLA - Tienda Virtual"}
+      description={"Tienda Virtual, venta de guitarras, instrumentos, GuitarLA"}
+    >
+      <main className="contenedor">
+        <h1 className="heading">Nuestra Colección</h1>
 
-
-
-
-    return (
-            <Layout
-                tittle={'GuitarLA - Tienda Virtual'}
-                description={'Tienda Virtual, venta de guitarras, instrumentos, GuitarLA'}
-            >
-                <main className="contenedor">
-                    <h1 className="heading">Nuestra Colección</h1>
-
-                    {guitarras?.map( guitarra => (
-                        <Guitarra
-                            key={guitarra.id}
-                            guitarra={guitarra.attributes}
-                        />
-                    ))}
-                </main>
-            </Layout>
-
-    )
+        <div className={styles.grid}>
+          {guitarras?.map((guitarra) => (
+            <Guitarra key={guitarra.id} guitarra={guitarra.attributes} />
+          ))}
+        </div>
+      </main>
+    </Layout>
+  );
 }
 
 export async function getServerSideProps() {
-    const respuesta = await fetch(`${process.env.API_URL}/guitarras?populate=imagen`)
-    const {data: guitarras} = await respuesta.json()
+  const respuesta = await fetch(
+    `${process.env.API_URL}/guitarras?populate=imagen`
+  );
+  const { data: guitarras } = await respuesta.json();
 
-    return {
-        props: {
-            guitarras
-        }
-    }
+  return {
+    props: {
+      guitarras,
+    },
+  };
 }
